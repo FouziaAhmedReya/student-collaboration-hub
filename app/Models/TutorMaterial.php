dc97c22ecb2c2308d295045f7f5ad59cb59e4463
FouziaAhmedReya <159718701+FouziaAhmedReya@users.noreply.github.com>
2026-08-23 23:29:35 +0600

    Add Tutor and TutorMaterial models with migration for database tables

---
+++ app/Models/TutorMaterial.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TutorMaterial extends Model
+{
+    protected $fillable = [
+        'tutor_id',
+
+        'title',
+
+        'file_name',
+
+        'file_url',
+
+        'cloudinary_public_id',
+
+        'resource_type',
+    ];
+
+
+    public function tutor(): BelongsTo
+    {
+        return $this->belongsTo(
+            Tutor::class
+        );
+    }
+}
